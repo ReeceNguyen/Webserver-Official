df26cdcd1d82abcb3f5d603aae48541ec66f506e
ReeceNguyen <minhthai160501@outlook.com.vn>
2024-02-01 21:31:47 +0700

    version 1-2-2024-21.31

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,11 @@
 const getHomepage = (req, res) => {
-  var ipAddress = "http://" + process.env.SERVER_IP + ":" + process.env.PORT + "/";
+  var ipAddress = "http://" + process.env.SERVER_IP + ":" + process.env.PORT;
   res.render("home.ejs", { ipAddress });
 };
 
 const getLoginpage = (req, res) => {
   var ipAddress =
-    "http://" + process.env.SERVER_IP + ":" + process.env.PORT + "/login";
+    "http://" + process.env.SERVER_IP + ":" + process.env.PORT;
   res.render("loginpage.ejs", { ipAddress });
 };
 
