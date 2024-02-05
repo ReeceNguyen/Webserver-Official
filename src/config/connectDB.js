76879210c10aa6d0c24d8cc6da1ab834ccfb851e
ReeceNguyen <minhthai160501@outlook.com.vn>
2024-02-05 23:46:52 +0700

    version 5/2/2024 23.46 add user database

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+// STATION 1 DATABASE
 var sqlcon = mysql.createConnection({
   host: process.env.SQL_HOST,
   user: process.env.SQL_USER,
@@ -6,6 +7,7 @@ var sqlcon = mysql.createConnection({
   database: process.env.SQL_STATION_1,
   dateStrings: true,
 });
+// STATION 2 DATABASE
 var sqlcon_s2 = mysql.createConnection({
   host: process.env.SQL_HOST,
   user: process.env.SQL_USER,
@@ -13,8 +15,17 @@ var sqlcon_s2 = mysql.createConnection({
   database: process.env.SQL_STATION_2,
   dateStrings: true,
 });
+// ACCOUNT DATA
+var sqlaccount = mysql.createConnection({
+  host: process.env.SQL_HOST,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_KEY,
+  database: process.env.SQL_ACCOUNT,
+  dateStrings: true,
+});
 
 module.exports = {
     sqlcon,
-    sqlcon_s2
+    sqlcon_s2,
+    sqlaccount,
 }
